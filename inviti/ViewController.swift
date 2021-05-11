202a4a2ca5d789ae417fcb8cf8eca8b63b2fb5f6
Hannah.C <java8300@gmail.com>
2021-05-11 18:02:08 +0800

    add main Xcode files and .gitignore

---
+++ inviti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  inviti
+//
+//  Created by Hannah.C on 11.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
