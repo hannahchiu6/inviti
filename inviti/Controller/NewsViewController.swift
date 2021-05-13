16b6ef374d5215197471d1e2674910f80f3b8105
Hannah.C <java8300@gmail.com>
2021-05-13 13:43:41 +0800

    Created NavigationBar

---
+++ inviti/Controller/NewsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewsViewController.swift
+//  inviti
+//
+//  Created by Hannah.C on 13.05.21.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
